b1e93d0e8bd1b4004a4303781b5336c935176706
Tim Scheuermann <tim@plusmid.net>
2020-07-24 12:50:59 +0200

    Added functionality to iterate over slices with *

--- uncertain.go
+++ uncertain.go
@@ -22,17 +22,17 @@ func Get(from interface{}, path ...interface{}) (interface{}, error) {
 }
 
 func get(from reflect.Value, path []interface{}) (reflect.Value, error) {
-	// no more path segments, this seems to be the value the user wants
-	if len(path) == 0 {
-		return from, nil
-	}
-
 	// dereference all (nested) pointers and interfaces
-	for from.Kind() == reflect.Ptr || from.Kind() == reflect.Interface {
-		if from.IsNil() {
+	for (from.Kind() == reflect.Ptr || from.Kind() == reflect.Interface) && !from.IsNil() {
+		if from.IsNil() && len(path) != 0 {
 			return reflect.Value{}, errors.New("unable to dereference nil pointer")
 		}
 		from = from.Elem()
+	}
+
+	// no more path segments, this seems to be the value the user wants
+	if len(path) == 0 {
+		return from, nil
 	}
 
 	// now we try all the different (supported) kinds of values
@@ -46,7 +46,44 @@ func get(from reflect.Value, path []interface{}) (reflect.Value, error) {
 		fallthrough
 
 	case reflect.Array, reflect.String:
-		// this is a slice, array, or string; use the path segment as an index
+		// this is a slice, array, or string
+
+		if path[0] == "*" {
+			// we'll iterate over the array/slice/string
+			n := from.Len()
+			sl := make([]reflect.Value, 0, n)
+			for i := 0; i < n; i++ {
+				val, err := get(from.Index(i), path[1:])
+				if err == nil {
+					sl = append(sl, val)
+				}
+			}
+			if len(sl) == 0 {
+				return reflect.Value{}, errors.New("found no usable values while iterating the slice")
+			}
+
+			// Check if they are all the same type
+			typ := sl[0].Type()
+			for _, val := range sl[1:] {
+				if val.Type() != typ {
+					typ = nil
+					break
+				}
+			}
+
+			// create a slice with the matching type or []interface{}
+			if typ == nil {
+				typ = reflect.TypeOf((*interface{})(nil)).Elem()
+			}
+			tsl := reflect.MakeSlice(reflect.SliceOf(typ), len(sl), len(sl))
+			for i, val := range sl {
+				tsl.Index(i).Set(val)
+			}
+
+			return tsl, nil
+		}
+
+		// use the path segment as an index
 		i, ok := anyToInt(path[0])
 		if !ok {
 			return reflect.Value{}, errors.New("path segment can't be interpreted as a number")
